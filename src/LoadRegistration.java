47dffaa09bc6c14bcfb386cd9aa78cdeca399109
djkier <djjustinekier@gmail.com>
2026-09-07 09:34:28 +0800

    Update ui, and back logic

--- src/LoadRegistration.java
+++ src/LoadRegistration.java
@@ -94,11 +94,11 @@ public class LoadRegistration {
     }
 
     public static void unliTextPromo() {
-        boolean isChoiceValid = false;
+        boolean isGoingBackToMainMenu = false;
 
         System.out.println(whiteSpace);
 
-        while(!isChoiceValid) {
+        while(!isGoingBackToMainMenu) {
             System.out.println("***  Unli Text Promos  ***");
             System.out.println("    [1] 1 Day for P10");
             System.out.println("    [2] 3 Days for P30");
@@ -112,50 +112,43 @@ public class LoadRegistration {
 
             switch (choice) {
                 case "1":
-                    subscriptionConfirmation("Unli Text", 1, 10, 1);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Text", 1, 10, 1);
                     break;
 
                 case "2":
-                    subscriptionConfirmation("Unli Text", 3, 30, 2);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Text", 3, 30, 2);
                     break;
 
                 case "3":
-                    subscriptionConfirmation("Unli Text", 5, 50, 3);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Text", 5, 50, 3);
                     break;
 
                 case "4":
-                    subscriptionConfirmation("Unli Text", 15, 140, 5);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Text", 15, 140, 5);
                     break;
 
                 case "5":
-                    subscriptionConfirmation("Unli Text", 30, 250, 10);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Text", 30, 250, 10);
                     break;
 
                 case "6":
+                    isGoingBackToMainMenu = true;
+                    System.out.println(whiteSpace);
                     break;
 
                 default:
                     System.out.println(whiteSpace);
                     System.out.println("Invalid choice!");
             }
-
-            if (choice.equals("1") ||
-                    choice.equals("2") ||
-                    choice.equals("3") ||
-                    choice.equals("4") ||
-                    choice.equals("5") ||
-                    choice.equals("6")) {
-                isChoiceValid = true;
-            }
         }
     }
 
     public static void unliCallPromo() {
-        boolean isChoiceValid = false;
+        boolean isGoingBackToMainMenu = false;
 
         System.out.println(whiteSpace);
 
-        while(!isChoiceValid) {
+        while(!isGoingBackToMainMenu) {
             System.out.println("***  Unli Call Promos  ***");
             System.out.println("    [1] 1 Day for P20");
             System.out.println("    [2] 3 Days for P50");
@@ -169,50 +162,43 @@ public class LoadRegistration {
 
             switch (choice) {
                 case "1":
-                    subscriptionConfirmation("Unli Call", 1, 20, 0);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call", 1, 20, 0);
                     break;
 
                 case "2":
-                    subscriptionConfirmation("Unli Call", 3, 50, 0);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call", 3, 50, 0);
                     break;
 
                 case "3":
-                    subscriptionConfirmation("Unli Call", 7, 100, 0);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call", 7, 100, 0);
                     break;
 
                 case "4":
-                    subscriptionConfirmation("Unli Call", 15, 180, 0);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call", 15, 180, 0);
                     break;
 
                 case "5":
-                    subscriptionConfirmation("Unli Call", 30, 300, 0);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call", 30, 300, 0);
                     break;
 
                 case "6":
+                    isGoingBackToMainMenu = true;
+                    System.out.println(whiteSpace);
                     break;
 
                 default:
                     System.out.println(whiteSpace);
                     System.out.println("Invalid choice!");
             }
-
-            if (choice.equals("1") ||
-                    choice.equals("2") ||
-                    choice.equals("3") ||
-                    choice.equals("4") ||
-                    choice.equals("5") ||
-                    choice.equals("6")) {
-                isChoiceValid = true;
-            }
         }
     }
 
     public static void unliCallTextPromo() {
-        boolean isChoiceValid = false;
+        boolean isGoingBackToMainMenu = false;
 
         System.out.println(whiteSpace);
 
-        while(!isChoiceValid) {
+        while(!isGoingBackToMainMenu) {
             System.out.println("***  Unli Call & Text Promos  ***");
             System.out.println("    [1] 1 Day for P30");
             System.out.println("    [2] 3 Days for P70");
@@ -226,50 +212,43 @@ public class LoadRegistration {
 
             switch (choice) {
                 case "1":
-                    subscriptionConfirmation("Unli Call & Text", 1, 30, 1);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call & Text", 1, 30, 1);
                     break;
 
                 case "2":
-                    subscriptionConfirmation("Unli Call & Text", 3, 70, 2);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call & Text", 3, 70, 2);
                     break;
 
                 case "3":
-                    subscriptionConfirmation("Unli Call & Text", 7, 120, 3);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call & Text", 7, 120, 3);
                     break;
 
                 case "4":
-                    subscriptionConfirmation("Unli Call & Text", 15, 200, 5);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call & Text", 15, 200, 5);
                     break;
 
                 case "5":
-                    subscriptionConfirmation("Unli Call & Text", 30, 350, 10);
+                    isGoingBackToMainMenu = subscriptionConfirmation("Unli Call & Text", 30, 350, 10);
                     break;
 
                 case "6":
+                    isGoingBackToMainMenu = true;
+                    System.out.println(whiteSpace);
                     break;
 
                 default:
                     System.out.println(whiteSpace);
                     System.out.println("Invalid choice!");
             }
-
-            if (choice.equals("1") ||
-                    choice.equals("2") ||
-                    choice.equals("3") ||
-                    choice.equals("4") ||
-                    choice.equals("5") ||
-                    choice.equals("6")) {
-                isChoiceValid = true;
-            }
         }
     }
 
     public static void surfPromo() {
-        boolean isChoiceValid = false;
+        boolean isGoingBackToMainMenu = false;
 
         System.out.println(whiteSpace);
 
-        while(!isChoiceValid) {
+        while(!isGoingBackToMainMenu) {
             System.out.println("***  Surf Promos  ***");
             System.out.println("    [1] 1GB for P20");
             System.out.println("    [2] 2GB for P40");
@@ -283,124 +262,110 @@ public class LoadRegistration {
 
             switch (choice) {
                 case "1":
-                    surfSubscriptionConfirmation(1, 20);
+                    isGoingBackToMainMenu = surfSubscriptionConfirmation(1, 20);
                     break;
 
                 case "2":
-                    surfSubscriptionConfirmation(2, 40);
+                    isGoingBackToMainMenu = surfSubscriptionConfirmation(2, 40);
                     break;
 
                 case "3":
-                    surfSubscriptionConfirmation(5, 90);
+                    isGoingBackToMainMenu = surfSubscriptionConfirmation(5, 90);
                     break;
 
                 case "4":
-                    surfSubscriptionConfirmation(10, 170);
+                    isGoingBackToMainMenu = surfSubscriptionConfirmation(10, 170);
                     break;
 
                 case "5":
-                    surfSubscriptionConfirmation(20, 300);
+                    isGoingBackToMainMenu = surfSubscriptionConfirmation(20, 300);
                     break;
 
                 case "6":
+                    isGoingBackToMainMenu = true;
+                    System.out.println(whiteSpace);
                     break;
 
                 default:
                     System.out.println(whiteSpace);
                     System.out.println("Invalid choice!");
             }
-
-            if (choice.equals("1") ||
-                    choice.equals("2") ||
-                    choice.equals("3") ||
-                    choice.equals("4") ||
-                    choice.equals("5") ||
-                    choice.equals("6")) {
-                isChoiceValid = true;
-            }
         }
     }
 
-    public static void subscriptionConfirmation(String promoType, int days, int price, int gb) {
-        boolean isChoiceValid = false;
+    public static boolean subscriptionConfirmation(String promoType, int days, int price, int gb) {
         String daysStr = days > 1 ? "days" : "day";
+        String gbStr = gb > 0 ? " with " + gb + "GB data for free." : ".";
 
         System.out.println(whiteSpace);
 
-        while(!isChoiceValid) {
-            if (gb > 0) {
-                System.out.println("->  Get " + days + " " + daysStr + " " + promoType + " Promo for only P" + price + " with " + gb + "GB data for free.");
-            }
-            else {
-                System.out.println("->  Get " + days + " " + daysStr + " " + promoType + " Promo for only P" + price + ".");
-            }
-
+        while(true) {
+            System.out.println("->  Get " + days + " " + daysStr + " " + promoType + " Promo for only P" + price + gbStr);
             System.out.println("   [1] Subscribe");
             System.out.println("   [2] Back");
             System.out.println("   [3] Exit");
             System.out.print("Enter choice: ");
             String subChoice = scanner.nextLine();
+            System.out.println(whiteSpace);
 
             switch (subChoice){
                 case "1":
                     if (balance >= price) {
                         balance -= price;
                         System.out.println("You are now subscribed to " + promoType + " " + price + " Promo...");
-                        isChoiceValid = true;
                     } else {
-                        System.out.println("You don't have enough balance. Try borrowing load to continue.");
+                        System.out.printf("You only have P%.2f in your account. Try borrowing load to continue.%n", balance);
                     }
-                    break;
+                    backToMainMenuPrompt();
+                    System.out.println(whiteSpace);
+                    return true;
 
                 case "2":
-                    isChoiceValid = true;
-                    break;
+                    return false;
 
                 case "3":
                     exitFunction();
                     break;
 
                 default:
-                    System.out.println(whiteSpace);
                     System.out.println("Invalid choice!");
             }
         }
     }
 
-    public static void surfSubscriptionConfirmation(int gb, int price) {
-        boolean isChoiceValid = false;
+    public static boolean surfSubscriptionConfirmation(int gb, int price) {
 
         System.out.println(whiteSpace);
 
-        while(!isChoiceValid) {
+        while(true) {
             System.out.println("->  Get " + gb + "GB Surf Promo for only P" + price + ".");
             System.out.println("   [1] Subscribe");
             System.out.println("   [2] Back");
             System.out.println("   [3] Exit");
             System.out.print("Enter choice: ");
             String subChoice = scanner.nextLine();
+            System.out.println(whiteSpace);
 
             switch (subChoice){
                 case "1":
                     if (balance >= price) {
                         balance -= price;
                         System.out.println("You are now subscribed to " + gb + "GB Surf Promo...");
-                        isChoiceValid = true;
                     } else {
-                        System.out.println("You don't have enough balance. Try borrowing load to continue.");
+                        System.out.printf("You only have P%.2f in your account. Try borrowing load to continue.%n", balance);
                     }
-                    break;
+                    backToMainMenuPrompt();
+                    System.out.println(whiteSpace);
+                    return true;
 
                 case "2":
-                    isChoiceValid = true;
-                    break;
+                    return false;
 
                 case "3":
                     exitFunction();
                     break;
 
                 default:
-                    System.out.println(whiteSpace);
                     System.out.println("Invalid choice!");
             }
         }
@@ -408,9 +373,8 @@ public class LoadRegistration {
 
     public static void checkBalance() {
         System.out.println(whiteSpace);
-        System.out.println("Your current balance is P" + balance);
-        System.out.println("Press Enter to go back to the main menu.");
-        scanner.nextLine();
+        System.out.printf("Your current balance is P%.2f%n", balance);
+        backToMainMenuPrompt();
         System.out.println(whiteSpace);
     }
 
@@ -424,34 +388,34 @@ public class LoadRegistration {
             System.out.println("    [1] Borrow P10");
             System.out.println("    [2] Borrow P20");
             System.out.println("    [3] Borrow P50");
-            System.out.println("    [4] Back");
+            System.out.println("    [4] Borrow P100");
+            System.out.println("    [5] Back");
 
             System.out.print("Enter choice: ");
             String choice = scanner.nextLine();
 
             switch (choice) {
                 case "1":
-                    balance += 10;
-                    System.out.println("You borrowed P10 load.");
-                    System.out.println("Your new balance is P" + balance);
+                    borrowLoadSuccess(10);
                     isChoiceValid = true;
                     break;
 
                 case "2":
-                    balance += 20;
-                    System.out.println("You borrowed P20 load.");
-                    System.out.println("Your new balance is P" + balance);
+                    borrowLoadSuccess(20);
                     isChoiceValid = true;
                     break;
 
                 case "3":
-                    balance += 50;
-                    System.out.println("You borrowed P50 load.");
-                    System.out.println("Your new balance is P" + balance);
+                    borrowLoadSuccess(50);
                     isChoiceValid = true;
                     break;
 
                 case "4":
+                    borrowLoadSuccess(100);
+                    isChoiceValid = true;
+                    break;
+
+                case "5":
                     isChoiceValid = true;
                     break;
 
@@ -464,9 +428,22 @@ public class LoadRegistration {
         System.out.println(whiteSpace);
     }
 
+    public static void borrowLoadSuccess(int price) {
+        System.out.println(whiteSpace);
+        balance += price;
+        System.out.println("You successfully borrowed P" + price + " load.");
+        System.out.printf("Your new balance is P%.2f%n", balance);
+        backToMainMenuPrompt();
+    }
+
+    public static void backToMainMenuPrompt() {
+        System.out.println("[Press \"Enter\" to return to main menu.]");
+        scanner.nextLine();
+    }
+
     public static void exitFunction() {
         System.out.println("Thank you for using our service.");
-        System.out.println("Exiting. . .");
+        System.out.println("Exiting...");
         System.exit(0); // Exit JVM
     }
 
